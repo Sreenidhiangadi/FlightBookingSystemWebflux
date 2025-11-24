a808d4ef409f9f6aecea1aaf791ea676cdbfe721
Sreenidhi Angadi <angadisreenidhi@gmail.com>
2025-11-24 22:29:37 +0530

    added flightBookingWebfluxApplicationTests.java

--- FlightBookingWebflux/src/test/java/com/flightapp/FlightBookingWebfluxApplicationTests.java
+++ FlightBookingWebflux/src/test/java/com/flightapp/FlightBookingWebfluxApplicationTests.java
@@ -3,11 +3,17 @@ package com.flightapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class FlightBookingWebfluxApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @Test
+    void mainMethodRuns() {
+        FlightBookingWebfluxApplication.main(
+                new String[]{"--spring.main.web-application-type=none"}
+        );
+    }
 }
